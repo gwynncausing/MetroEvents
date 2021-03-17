cbeb32b5334e141a4c1c9eeb5d31f7a5dda29d18
gwynncausing <57838522+gwynncausing@users.noreply.github.com>
2021-03-17 18:48:48 +0800

    Add app

--- metroEvents/app/views.py
+++ metroEvents/app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import View
+from django.http import HttpResponse
 
 # Create your views here.
+
+class LoginView(View):
+  def get(self,request):
+    # return HttpResponse("Hello, world. You're at the polls index.")
+    
+    return render(request, 'app/home.html')
