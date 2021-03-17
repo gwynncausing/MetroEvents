a9fce1e14aca63b727b10570bac22acee8c0c206
Shangcasm <58620029+shangcasm@users.noreply.github.com>
2021-03-17 15:19:33 +0800

    connecting django to database

--- metroEvents/metroEvents/settings.py
+++ metroEvents/metroEvents/settings.py
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'metroEvents.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'metro',
+        'USER': 'group9@group9-metroevents',
+        'PASSWORD': 'ACCL9Team!',
+        'HOST': 'group9-metroevents.mysql.database.azure.com',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
