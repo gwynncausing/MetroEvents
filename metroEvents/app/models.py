df474406e38316b1a305d6c4410f064595846e5f
Shangcasm <58620029+shangcasm@users.noreply.github.com>
2021-03-18 00:02:25 +0800

    Created model User

--- metroEvents/app/models.py
+++ metroEvents/app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    userId = models.ForeignKey('app.User',on_delete=models.PROTECT)
+    first_name = models.CharField(max_length = 50)
+    last_name = models.CharField(max_length = 50)
+    date_created= models.DateTimeField()
+    username = models.CharField(max_length = 30)
+    password = models.CharField(max_length = 30)
+    birthdate = models.DateField()
+    address = models.CharField(max_length = 50)
+    userType = models.CharField(max_length = 50)
