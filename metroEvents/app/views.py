47ca120ddf3f83e7271526a7c6bb3e1189d8374d
gwynncausing <57838522+gwynncausing@users.noreply.github.com>
2021-03-18 20:21:07 +0800

    Update Register
    users can now register, user redirects to login page if success

--- metroEvents/app/views.py
+++ metroEvents/app/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
 from django.http import HttpResponse
+
+from django.contrib.auth import authenticate, login, logout
+
+from .forms import CreateUserForm
 
 # Create your views here.
 
@@ -10,7 +14,20 @@ class LoginView(View):
 
 class RegistrationView(View):
   def get(self,request):
-    return render(request, 'app/registration.html')
+    form = CreateUserForm()
+    
+    context = {'form': form}
+    return render(request, 'app/registration.html', context)
+  
+  def post(self, request):
+    form = CreateUserForm(request.POST)
+    print("form is valid: " , form.is_valid())
+    if form.is_valid():
+      form.save()
+      return redirect('app:login')
+    print(form.errors)
+    return HttpResponse("error!")
+
 
 class CreateEventView(View):
   def get(self,request):
