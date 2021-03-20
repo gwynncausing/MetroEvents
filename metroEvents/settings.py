93e582c92f1b0eedd9eaae0f6788bf009a2dab5c
gwynncausing <57838522+gwynncausing@users.noreply.github.com>
2021-03-21 01:38:18 +0800

    Add staticfiles

--- metroEvents/settings.py
+++ metroEvents/settings.py
@@ -144,16 +144,18 @@ TEMPLATES = [
     },
 ]
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 # STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
+#     BASE_DIR / "static",
+#     '/var/www/static/',
 # ]
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/var/www/static/',
-]
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/'
