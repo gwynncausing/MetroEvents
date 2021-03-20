fa725992b8d1e45afeadeb44d01d3de9b05208e8
gwynncausing <57838522+gwynncausing@users.noreply.github.com>
2021-03-21 01:36:01 +0800

    Update settings.py

--- metroEvents/settings.py
+++ metroEvents/settings.py
@@ -148,6 +148,10 @@ TEMPLATES = [
 #     os.path.join(BASE_DIR, 'static'),
 # ]
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/var/www/static/',
+]
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
